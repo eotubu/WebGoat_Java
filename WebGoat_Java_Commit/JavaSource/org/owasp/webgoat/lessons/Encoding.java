8453596840dee6fc4eb76ffe34ce91fb6c2149cc
otubu <otubu@host.docker.internal>
2021-07-15 14:40:09 -0500

    test2

--- WebGoat_Java_Commit/JavaSource/org/owasp/webgoat/lessons/Encoding.java
+++ WebGoat_Java_Commit/JavaSource/org/owasp/webgoat/lessons/Encoding.java
@@ -484,7 +484,7 @@ public class Encoding extends LessonAdapter
 		try
 		{
 
-			PBEParameterSpec ps = new javax.crypto.spec.PBEParameterSpec( salt, 20000000 );
+			PBEParameterSpec ps = new javax.crypto.spec.PBEParameterSpec( salt, 18000000 );
 
 			SecretKeyFactory kf = SecretKeyFactory.getInstance( "PBEWithMD5AndDES" );
 
